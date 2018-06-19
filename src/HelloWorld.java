ccd99c37f81e7299bbfdb4a2602b9499d8e3ff6b
IEUser <ieuser@mail.com>
2018-06-19 16:58:25 -0700

    'update_message'

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hellow Codegym! and Git");
+        System.out.println("Hellow Codegym!, Java and Git");
     }
 }
